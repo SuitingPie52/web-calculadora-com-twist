67d9c80eb58642e0ed7399d04d90e1aff60e1987
Gabriel Paiva <gabrielanpaiva2005@gmail.com>
2024-04-18 09:05:31 +0000

    implementing get result function

--- ResultController.go
+++ ResultController.go
@@ -2,6 +2,9 @@ package main
 
 import(
 	"net/http"
+	"strings"
+	"strconv"
+	"encoding/json"
 	
 )
 
@@ -19,7 +22,63 @@ var GetFunctions = map[string]func(http.ResponseWriter, *http.Request) {
 
 func GetResult (res http.ResponseWriter, req *http.Request) {
 
-	res.Write([]byte("achou"))
+	op := req.URL.Query().Get("op")
+
+	noDashOp := strings.ReplaceAll(op, " ", "")
+
+	var CheckOperators = map[bool]string{}
+
+	for k := range TableCalculable {
+	
+		CheckOperators[strings.Contains(noDashOp, k)] = k
+	
+	}
+	
+	if !isAnyOperationValid(CheckOperators) {
+	
+		WriteInvalidExpression(res, op)
+		return
+		
+	}
+	
+	slicedOp := strings.Split(noDashOp, CheckOperators[true])
+	
+	if len(slicedOp) > 2 {
+	
+		WriteInvalidExpression(res, op)
+		return
+	
+	}
+		
+	var1, err1 := strconv.ParseFloat(slicedOp[0], 64)
+	var2, err2 := strconv.ParseFloat(slicedOp[1], 64)
+	
+	if err1 != nil || err2 != nil {
+	
+		WriteInvalidExpression(res, op)
+		return		
+	
+	}
+	
+	s := TableCalculable[CheckOperators[true]].Calculate(var1, var2, op)
+	
+	res.Write(CalculableToJson(s))
+	
+	return
+
+}
+
+func isAnyOperationValid (m map[bool]string)bool {
+
+	return m[true] != ""
+
+}
+
+func WriteInvalidExpression(res http.ResponseWriter, op string) {
+
+	e := map[string]string{"result": "invalid expression", "op": op}
+	j, _ := json.Marshal(e)
+	res.Write(j)
 	return
 
 }
